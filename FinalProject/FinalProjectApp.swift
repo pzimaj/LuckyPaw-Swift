ef264ce0baedac500885e1b4270934badff3b962
Marita Kresic <mk7333@g.rit.edu>
2022-11-18 21:14:03 +0100

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Marita Kresic on 18.11.2022..
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
